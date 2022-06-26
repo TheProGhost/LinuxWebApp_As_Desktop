a829989f23403454fc2c644eed256b309bad4aa8
TheProGhost <manan05vyas99.com>
2022-06-26 21:01:58 +0530

    Running the function

--- Launch_Whatsapp.sh
+++ Launch_Whatsapp.sh
@@ -1,18 +1,25 @@
 #!/bin/zsh
 
-launched=`wmctrl -lx | grep firefox | grep WhatsApp | cut -d ' ' -f3 | tail -n 1`
 
-echo "${launched}"
-
+# launch WhatsApp function
 launch_WhatsApp(){
-    echo " launching WhatsApp"
+    wmctrl -s 1
+    firefox --new-tab -url web.whatsapp.com
+    wmctrl -r firefox -b toggle,fullscreen
 }
 
-if [[ $launched == "" ]]; then
-launch_WhatsApp()
+launch_WhatsApp
 
+# checking if the WhatsApp is launched or not
+# launched=`wmctrl -lx | grep firefox | grep WhatsApp | cut -d ' ' -f3 | tail -n 1`
+
+# echo "${launched}"
+
+# if [ $launched == "" ]
+# then
+# launch_WhatsApp()
+# fi
 # else
 # output =`wmctrl -s ${launched}`
 # echo "${output}"
-
-fi
+#fi
