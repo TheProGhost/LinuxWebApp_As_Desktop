880e805a2b27e86c6efcd26dc4ac4150b9dd294f
TheProGhost <manan05vyas99.com>
2022-06-26 22:55:16 +0530

    Finally working the function

--- Launch_Whatsapp.sh
+++ Launch_Whatsapp.sh
@@ -7,11 +7,14 @@ launch_WhatsApp(){
     # launching whatsapp in firefox
     firefox --new-window 'web.whatsapp.com'
     # getting the Window_Id of the window with WhatsApp    
-    launched_whatsapp = `wmctrl -lx | grep firefox | grep WhatsApp | cut -d '' -f1` 
-    # toggle the window to full screen
-    wmctrl -i -r $launched_whatsapp -b toggle,fullscreen
+    # `wmctrl -lx | grep firefox | grep WhatsApp | cut -d ' ' -f1` 
+
+    sleep 0.6
+    echo "After sleep"    # toggle the window to full screen
+    wmctrl -i -r `wmctrl -lx | grep firefox | grep WhatsApp | cut -d ' ' -f1` -b toggle,fullscreen
+    echo " After full scrren"
     # movinf window to the new workspace
-    wmctrl -i -r 
+    wmctrl -i -r `wmctrl -lx | grep firefox | grep WhatsApp | cut -d ' ' -f1` -t 1
 }
 
 launch_WhatsApp
